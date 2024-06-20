b867e9947c3ec89345715ce60655e0f234b8bbd1
AmanGusain2003 <amangusain2003@gmail.com>
2024-06-20 23:07:58 +0530

    Fixed image size issue

--- frontend/src/components/MessageCard/Message.jsx
+++ frontend/src/components/MessageCard/Message.jsx
@@ -5,8 +5,20 @@ const Message = ({ message, isSender, moodColor, emoji }) => {
   return (
     <Flex direction="row" alignItems="center" maxWidth="70%" alignSelf={isSender ? 'flex-end' : 'flex-start'} mb={2}>
       <Text fontSize="2xl" mr={2}>{emoji}</Text>
-      <Box borderRadius="lg" p={3} bg={moodColor} color="white">
-        <Text dangerouslySetInnerHTML={{ __html: message.text }}></Text>
+      <Box 
+        borderRadius="lg" 
+        p={3} 
+        bg={moodColor} 
+        color="white" 
+        maxHeight="200px"  // Limit the height of the message box
+        maxWidth="300px"  // Limit the width of the message box
+        overflow="auto"  // Enable scrolling if content exceeds the height
+      >
+        <Text 
+          dangerouslySetInnerHTML={{ 
+            __html: message.text.replace(/<img /g, '<img style="max-width: 100%; max-height: 100%; object-fit: contain;" ')
+          }}
+        />
       </Box>
     </Flex>
   );
